45b5078b42351b5b0a3e6f7560d339e2a4356e3a
CAMARA <88945535+camara92@users.noreply.github.com>
2023-07-05 15:54:46 +0200

    afficher de la console des données de la database mongodb - initialisation de l'adresse IP, .env pour sécuriser les données personnelles

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv'
 import { MongoClient, ServerApiVersion } from 'mongodb';
 const app = express()
-const port = 4000;
+const port = 8000;
 dotenv.config(); 
 const uri= process.env.STRING_URI; 
 // const uri = "mongodb+srv://camara92:Wxtaze9z1K5knzlq@cluster0.bbhrjyy.mongodb.net/?retryWrites=true&w=majority";
@@ -11,17 +11,22 @@ const uri= process.env.STRING_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, 
   serverApi:  ServerApiVersion.v1 });
-  client.connect((err)=> {
-    // const collection = client.db("test").collection("devices");
-    console.log("base de données connecté ");
-    client.close(); 
+  app.get('/', (_, res)=> {
+  client.connect((err, db)=> {
+      // etablir 
+      console.log("base de données connectée ");
+    if(err|| !db){
+        return false; 
+    }
+    // connexion a la base : 
+   db.db = client.db("blog").collection("post").find().toArray( function(err, resultat){
+    if(!err){ 
+        console.log(resultat)
+        res.status(200).send(resultat);
+    }
+   });
+    //client.close(); 
   });
-
-
-
-
-app.get('/', (_, res)=> {
-    res.send("Bonjour daouda dadzada")
 })
 app.listen(port, ()=> {
     console.log("Serveur sur le port "+ port);
