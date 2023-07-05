d092bb8477bb54e4cf57057cccc76aa3bdfb6704
CAMARA <88945535+camara92@users.noreply.github.com>
2023-07-05 02:27:01 +0200

    ajout de express js nodemon, babel un peu de routage

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Salut")
+import express from 'express';
+// const express = require('express');
+const app = express()
+const port = 4000;
+app.get('/', (_, res)=> {
+    res.send("Bonjour daouda dadzada")
+})
+app.listen(port, ()=> {
+    console.log("Serveur sur le port "+ port);
+})
