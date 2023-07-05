432c482456189e7691346bb86431c97f8b8ed313
CAMARA <88945535+camara92@users.noreply.github.com>
2023-07-05 14:29:44 +0200

    package dotenv

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from 'express';
 // const express = require('express');
+import dotenv from 'dotenv'
 import { MongoClient, ServerApiVersion } from 'mongodb';
 const app = express()
 const port = 4000;
-
-const uri = "mongodb+srv://camara92:Wxtaze9z1K5knzlq@cluster0.bbhrjyy.mongodb.net/?retryWrites=true&w=majority";
+dotenv.config(); 
+const uri= process.env.STRING_URI; 
+// const uri = "mongodb+srv://camara92:Wxtaze9z1K5knzlq@cluster0.bbhrjyy.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, 
   serverApi:  ServerApiVersion.v1 });
   client.connect((err)=> {
     // const collection = client.db("test").collection("devices");
-    console.log("base de données connectée ");
+    console.log("base de données connecté ");
     client.close(); 
   });
 
